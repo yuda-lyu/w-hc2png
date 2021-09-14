58101e287dbd2df97e699aeb46d5855d4b1a142d
yuda-lyu <firsemisphere@gmail.com>
2021-09-14 15:27:15 +0800

    modify: update pks

--- scla.mjs
+++ scla.mjs
@@ -1,5 +1,20 @@
-// import fs from 'fs'
+import fs from 'fs'
+import _ from 'lodash'
 import WHc2png from './src/WHc2png.mjs'
+
+
+function genPlotHtml(fp, b64) {
+    let h = `
+<html>
+    <head></head>
+    <body>
+        <img style="border:1px dashed #ddd;" src="data:image/png;base64, {b64}">
+    </body>
+</html>
+    `
+    h = h.replace('{b64}', b64)
+    fs.writeFileSync(fp, h, 'utf8')
+}
 
 
 async function test1() {
@@ -43,11 +58,29 @@ async function test1() {
         }
     }
     let b64 = await WHc2png(st.width, st.height, st.scale, st.opt)
-    console.log('test1', b64)
+    // console.log('test1', b64)
 
-    // fs.writeFileSync('test1.b64', b64)
+    // fs.writeFileSync('./test1.b64', b64)
+    genPlotHtml('./test1.html', b64)
 
 }
 test1()
+
+async function test2() {
+
+    let st = {
+        width: 190,
+        height: 560,
+        scale: 1,
+        opt: JSON.parse(_.trim(fs.readFileSync('./opt (9).json', 'utf8'))),
+    }
+    let b64 = await WHc2png(st.width, st.height, st.scale, st.opt)
+    // console.log('test2', b64)
+
+    genPlotHtml('./test2.html', b64)
+
+}
+test2()
+
 
 //node --experimental-modules --es-module-specifier-resolution=node scla.mjs
